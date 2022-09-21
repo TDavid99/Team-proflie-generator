bd10aef22a63f66d8b48515f22b161d241c1b7d4
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-09-21 10:48:52 -0400

    working on postions for team

---
+++ test/manager.test.js
@@ -0,0 +1,11 @@
+const Employee = require ('../lib/employee');
+const Manager = require('../lib/manager')
+
+Test("get mangager content", ()=> {
+    const manager = new Mangager(David)
+})
+
+Test("get office number", ()=> {
+    const testvalue = 100;
+    const e = new Manager("")
+})
