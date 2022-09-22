85a74ea2613a54ca0a270f9b7f8e8c85156a33aa
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-09-21 23:55:42 -0400

    not finish

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Team-proflie-generator
+
+# This application that usees both inquirer and jest to get information to delevop a Buisnees team 
--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const inquirer = require ('inquirer');
 const fs = require('fs');
 const path = require('path');
 const jest = require('jest');
+
+
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+
+
+const dist_DIR = path.resolve(__dirname, 'dist')
+const outputPath = path.join(dist_DIR, 'index.html');
+
+const renderm = require('./src/output')
