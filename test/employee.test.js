68b07f5dff435f994174e40ea8a3b3ff9f58755a
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-10-29 00:37:08 -0400

    employee postion and name

--- test/employee.test.js
+++ test/employee.test.js
@@ -2,20 +2,27 @@ const {test, expect} = require("@jest/globals");
 const Employee = require("../lib/Employee.js");
 
 test("creates an Employee Object", () => {
-    const employee = new Employee("Dave","1", "dave@email.com");
+    const employee = new Employee("jesscia","1", "jess@email.com");
  
-    expect(employee.name).toBe("Dave");
+    expect(employee.name).toBe("jessica");
     expect(employee.id).toBe("1");
-    expect(employee.email).toBe("dave@email.com");
+    expect(employee.email).toBe("jess@email.com");
     
 });
 
 test("get employee's name", () => {
-    const employee = new Employee("Dave", "1" ,"dave@email.com");
+    const employee = new Employee("jessica", "1" ,"jess@email.com");
     expect(employee.getname()).toEqual(employee.name.toString());
 
 });
 
 test("gets employee's ID", () => {
-const employee = new Employee("Dave")
-}) 
+const employee = new Employee("jessica")
+
+expect(employee.getid()).toEqual(employee.id.toString());
+});
+test("get employee postion", () => {
+    const employee = new Employee("jessica", "1", "jess@gmail.com");
+    
+    expect(employee.getpostion()).toEqual("Employee");
+});
