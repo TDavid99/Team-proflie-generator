50979f544cb60fb73bd9ba87b29a999e2bcd54ea
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-10 08:48:44 -0500

    team manager

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,3 +1,5 @@
+const Manager = require("../lib/Manager");
+
 // generates html with tesm profile
 const generateHtml = (teamManager, allEngineers, allInterns) => {
     return `<!DOCTYPE html>
@@ -9,7 +11,27 @@ const generateHtml = (teamManager, allEngineers, allInterns) => {
         <title>My Team</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         </head>
-        
-    </html>
-}
+        <body class="min-100-vh">
+        <header>
+        <h1 class ="bg-danger text -light mb-3 p-3 text-center"> My Team</h1>
+        <header>
+        <main>
+        <div class="contanier">
+
+        ${teamManager.map(
+            (manager) =>
+            `<div class="row justify-content-center">
+            <div class="col-4 m-2">
+            <div class="card">
+            <div class="card-body">
+            <h5 class="card-title">Manager</h5>
+            <p class="card-text"> Name: ${manager.name}</p>
+            <p class="card-text"> Employee ID: ${manager.id}</p>
+            <p class="card-text"> Email address: <a herf ="mailto:${manager.email}">${manager.email}">${manager.email}</a></p>
+            <p class="card-text"> Office number: ${manager.officeNumber}</p>
+            </
+
+        )}
+    </html>`;
+};
 
