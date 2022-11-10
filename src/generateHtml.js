3a7110dd593ee8bcf41958d099701cc9f2b38d2b
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-10 08:24:10 -0500

    working to generate user friendly

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,2 +1,15 @@
 // generates html with tesm profile
-const generateHtml = (teamMana)
+const generateHtml = (teamManager, allEngineers, allInterns) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        </head>
+        
+    </html>
+}
+
